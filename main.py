796dd558a931137f8ed8e780bc78ed314a3d1ce0
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-05-07 21:09:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ name1 = 'Это моя строка'
 name2 = 'Это новая строка'
 print (name1[0])
 print (name1[-1])
-print (name1[2:6])
+print (name1[2:5])
 print (name1[::-1])
 print (len(name1))
 print (name1 + name2)
